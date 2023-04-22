aa92154bfda4e2cd1a8d59f3cc6213e4d894062e
Marcus <marcus.petrihen@gmail.com>
2023-04-22 15:55:40 +0300

    angular material BrowserAnimationsModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from '../app/shared/material/material.module';
+
+// Reactive Form
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../app/shared/material/material.module';
 
 // Routing
 import { RoutingModule } from '../app/shared/routing/routing.module';
 
+// Component
 import { AppComponent } from './app.component';
 import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-new.component';
 
@@ -14,9 +20,10 @@ import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-ne
   imports: [
     BrowserModule,
     RoutingModule,
-    MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
