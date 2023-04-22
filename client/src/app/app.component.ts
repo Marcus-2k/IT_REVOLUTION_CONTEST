b5312c814a214cf4071d54e03fa37251ea95700c
Marcus <marcus.petrihen@gmail.com>
2023-04-23 01:32:07 +0300

    delete not used

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
 
-import { OpenDialogService } from './shared/service/open-dialog.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private openWindow: OpenDialogService) {}
-
   title = 'real-estate';
-
-  testFunction() {
-    console.log('Hello');
-  }
-
-  login() {
-    this.openWindow.openLoginWindow();
-  }
-  register() {
-    this.openWindow.openRegisterWindow();
-  }
 }
