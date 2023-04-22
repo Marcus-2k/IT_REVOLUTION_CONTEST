925545fd20bd8d5a9e7313187e6a446279ee7a62
darktend <Fredifox1935>
2023-04-22 18:36:56 +0300

    create component history-operations

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,9 +14,13 @@ import { RoutingModule } from '../app/shared/routing/routing.module';
 // Component
 import { AppComponent } from './app.component';
 import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-new.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import { HistoryOperationsComponent } from './layouts/history-operations/history-operations.component';
+
 
 @NgModule({
-  declarations: [AppComponent, RealEstateNewComponent],
+  declarations: [AppComponent, RealEstateNewComponent,   HistoryOperationsComponent,  ],
   imports: [
     BrowserModule,
     RoutingModule,
@@ -24,8 +28,10 @@ import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-ne
     ReactiveFormsModule,
     MaterialModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {};
