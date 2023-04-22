f1533a554b774880aef1f0010b6f57dad406241c
levko <levkovich.vlad.2004@gmail.com>
2023-04-23 01:17:22 +0300

    wsgi

--- server/config/settings.py
+++ server/config/settings.py
@@ -78,12 +78,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'project',
+#         'USER': 'user',
+#         'PASSWORD': 'user123'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project',
-        'USER': 'user',
-        'PASSWORD': 'user123'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -8,17 +8,26 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, sys.path.dirname(os.path.abspath(__file__)))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pizza.settings')
 
-if os.environ.get("DJANGO_SETTINGS_MODULE") == "server.settings.production":
-        from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
-        application = Sentry(get_wsgi_application())
-else:
-        get_wsgi_application()
+application = get_wsgi_application()
+
+
+# import os
+# import sys
+# from django.core.wsgi import get_wsgi_application
+#
+# sys.path.insert(0, sys.path.dirname(os.path.abspath(__file__)))
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.production')
+#
+# if os.environ.get("DJANGO_SETTINGS_MODULE") == "server.settings.production":
+#         from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+#         application = Sentry(get_wsgi_application())
+# else:
+#         get_wsgi_application()
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # application = get_wsgi_application()
