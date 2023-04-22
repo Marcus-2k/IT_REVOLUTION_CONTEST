cbce2bcf374d1e12d0ecd97543a8723f153c8434
Marcus <marcus.petrihen@gmail.com>
2023-04-22 15:39:42 +0300

    add reactive form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from '../app/shared/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Routing
 import { RoutingModule } from '../app/shared/routing/routing.module';
@@ -9,8 +10,14 @@ import { AppComponent } from './app.component';
 import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-new.component';
 
 @NgModule({
-  declarations: [AppComponent,  RealEstateNewComponent],
-  imports: [BrowserModule, RoutingModule, MaterialModule],
+  declarations: [AppComponent, RealEstateNewComponent],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
