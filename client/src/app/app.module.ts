069a3c1a3c8f61c799a325051e08051e9a36a4f3
darktend <Fredifox1935>
2023-04-22 15:31:43 +0300

    fix folder layout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { RoutingModule } from '../app/shared/routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { RealEstateComponent } from './layouts/real-estate/real-estate.component';
+import { RealEstateNewComponent } from './layouts/real-estate-new/real-estate-new.component';
 
 @NgModule({
-  declarations: [AppComponent, RealEstateComponent],
+  declarations: [AppComponent, RealEstateComponent, RealEstateNewComponent],
   imports: [BrowserModule, RoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
