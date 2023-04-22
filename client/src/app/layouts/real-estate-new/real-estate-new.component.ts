6147e096dc33aee53d153b95ee93715f663fe2bc
Kosta45 <konstanlebed8@gmail.com>
2023-04-22 18:48:32 +0300

    form create order

--- client/src/app/layouts/real-estate-new/real-estate-new.component.ts
+++ client/src/app/layouts/real-estate-new/real-estate-new.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-real-estate-new',
   templateUrl: './real-estate-new.component.html',
-  styleUrls: ['./real-estate-new.component.scss']
+  styleUrls: ['./real-estate-new.component.scss'],
 })
 export class RealEstateNewComponent {
-
+  selected: number = 0;
 }
