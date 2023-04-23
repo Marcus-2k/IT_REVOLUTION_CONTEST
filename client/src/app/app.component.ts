0814734c8cd0bc40c818e0da4e69e18a178b3c30
Marcus <marcus.petrihen@gmail.com>
2023-04-23 12:14:03 +0300

    get token from localStorage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/service/server/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+
+  ngOnInit(): void {
+    const potentialToken = localStorage.getItem('auth-token');
+    this.auth.setToken(potentialToken);
+
+    if (potentialToken !== null) {
+      // the user is authorized in the system
+      console.log('The user is authorized in the system');
+    } else {
+      // the user is not authorized in the system
+      console.log('The user is NOT authorized in the system');
+    }
+  }
+
   title = 'real-estate';
 }
